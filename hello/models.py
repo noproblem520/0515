136f38b1f14893151f7523fc3437f7cd98345768
noproblem520 <48038181+noproblem520@users.noreply.github.com>
2019-05-15 14:52:18 +0800

    Demo

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete = models.CASCADE)
+	Choice_text = models.CharField(max_length = 200)
+	votes = models.IntegerField(default = 0)
